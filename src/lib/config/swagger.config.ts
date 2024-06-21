ada6d510970e1e41316901858b4ce477be38c69f
semenov <semenov>
2024-06-21 11:35:21 +0400

    создание конфигов подключение к бд и swagge

---
+++ src/lib/config/swagger.config.ts
@@ -0,0 +1,7 @@
+import { DocumentBuilder } from "@nestjs/swagger";
+
+export const SwaggerConfig = new DocumentBuilder()
+    .setTitle('Мед компания')
+    .setDescription('API Мед компания')
+    .setVersion('1.0')
+    .build()
