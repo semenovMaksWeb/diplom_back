843890fbf1e71beaff4a7b7945e909d465c6aa7e
semenov <semenov>
2024-12-02 09:10:22 +0400

    task fix api

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -16,7 +16,7 @@ export class TaskService {
     private convertTask(taskEntity: TaskEntity[]) {
         const result = [];
         for (const e of taskEntity) {
-            result.push({ id: e.id, theme: e.theme, message: e.message, date_create: e.date_create, date_end: e.date_end, client_name: e.client.name, developer_name: e.developer.name, status_name: e.statusTask.name })
+            result.push({ id: e.id, theme: e.theme, message: e.message, date_create: e.date_create, date_end: e.date_end, client_name: e.client.name, developer_name: e.developer.name, status_name: e.statusTask.name, developer_id: e.developer.id })
         }
         return result;
     }
